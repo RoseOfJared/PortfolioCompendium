afda383364a6816ad220e4fed2482edc7b05254e
RoseOfJared <RoseOfJared@outlook.com>
2019-05-09 17:11:21 -0400

    CollapsableScriptableObject work
    -Implemented CollapsableScriptableObject, allowing for in-editor fixing.

---
+++ Projects/CodersWorkshop/Assets/Scripts/Scriptable Bullshit/PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New PlayerStats", menuName="Player Stats")]
+public class PlayerStats : ScriptableObject
+{
+    [SerializeField]
+    private string className;
+    [SerializeField]
+    private string description;
+    [SerializeField]
+    private Sprite icon;
+    [SerializeField]
+    private int startingGold;
+    [SerializeField]
+    private int physicalAttack;
+    [SerializeField]
+    private int magicalAttack;
+}
---
+++ Projects/CodersWorkshop/Assets/Scripts/Scriptable Bullshit/TestPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    public PlayerStats playerStats;
+
+    /* Test to see how CollapsableScriptableObjects function in a list.
+        In short: they don't. Might be able to search for a fix, who knows
+     */
+    //public List<PlayerStats> testPlayerStats = new List<PlayerStats>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
