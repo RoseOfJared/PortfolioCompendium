be1ca4646c61ce05b31def315bd31521e273ef26
RoseOfJared <RoseOfJared@outlook.com>
2019-05-24 11:43:34 -0400

    Well, Progress regardless
    -LWRP is implemeted
    -Shader Graph is implemented, although trying to follow this particular tutorial is easier said than done. I wanna get it done and fixed by tonight, hopefully.

---
+++ Projects/CodersWorkshop/Assets/Materials/Toon/MainLight.hlsl
@@ -0,0 +1,21 @@
+﻿#ifndef MAINLIGHT_INCLUDED
+#define MAINLIGHT_INCLUDED
+
+float3 _Direction;
+float3 _Color;
+float _Attenuation;
+
+void GetLightingInformation_float(out float3 Direction, out float3 Color, out float Attenuation)
+{
+    #ifdef SHADERGRAPH_PREVIEW
+        Direction = float3(-0.5,0.5,-0.5);
+        Color = float3(1,1,1);
+        Attenuation = 0.4;
+    #else
+        Light light = GetMainLight();
+        Direction = light.direction;
+        Attenuation = light.distanceAttenuation;
+        Color = light.color;
+    #endif
+}
+#endif
