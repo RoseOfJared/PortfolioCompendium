443ec61f1f2b7a75faa0090b6f0ee9ed9da33d47
RoseOfJared <RoseOfJared@outlook.com>
2019-05-24 10:18:13 -0400

    Extensions, and then the worry
    -Implemented a couple extentsions
    -Gonna switch the project over to LWRP, committing now so as to avoid all this work going away

--- Projects/CodersWorkshop/Assets/Scripts/RoseExtensions.cs
+++ Projects/CodersWorkshop/Assets/Scripts/RoseExtensions.cs
@@ -1,18 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Link for With functions: https://twitter.com/JfranMora/status/1130868678277439490
 
-public class RoseExtensions : MonoBehaviour
+public static class RoseExtensions 
 {
-    // Start is called before the first frame update
-    void Start()
+    #region With() Functions
+    /*Purpose of functions is to replace element(s) in Vector2/3 and Color */
+
+    public static Vector2 With(this Vector2 orig, float? x = null, float? y = null)
     {
-        
+        if(x.HasValue) orig.x = x.Value;
+        if(y.HasValue) orig.y = y.Value;
+        return orig;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static Vector3 With(this Vector3 orig, float? x = null, float? y = null, float? z = null)
     {
-        
+        if(x.HasValue) orig.x = x.Value;
+        if(y.HasValue) orig.y = y.Value;
+        if(z.HasValue) orig.z = z.Value;
+        return orig;
     }
+
+    public static Color With(this Color orig, float? r = null, float? g = null, float? b = null, float? a = null)
+    {
+        if(r.HasValue) orig.r = r.Value;
+        if(g.HasValue) orig.g = g.Value;
+        if(b.HasValue) orig.b = b.Value;
+        if(a.HasValue) orig.a = a.Value;
+        return orig;
+    }
+
+    #endregion
+
+    #region String extensions
+
+    public static string Colored(this string message, Colors color)
+    {
+        return string.Format("<color={0}>{1}</color>", color.ToString(), message);
+    }
+
+    public static string Colored(this string message, string colorCode)
+    {
+        return string.Format("<color={0}>{1}</color>", colorCode, message);
+    }
+
+    public static string Sized(this string message, int size)
+    {
+        return string.Format("<size={0}>{1}</size>", size, message);
+    }
+
+    public static string Bold(this string message)
+    {
+        return string.Format("<i>{0}</i>", message);
+    }
+
+    public static string Italics(this string message)
+    {
+        return string.Format("<i>{0}</i>", message);
+    }
+
+    #endregion
+
+
+
+
+}
+
+public enum Colors{
+    aqua,
+	black,
+	blue,
+	brown,
+	cyan,
+	darkblue,
+	fuchsia,
+	green,
+	grey,
+	lightblue,
+	lime,
+	magenta,
+	maroon,
+	navy,
+	olive,
+	purple,
+	red,
+	silver,
+	teal,
+	white,
+	yellow
 }
---
+++ Projects/CodersWorkshop/Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Source link: https://twitter.com/BinaryImpactG/status/1128244218940592129
+
+public class SimpleRotate : MonoBehaviour
+{
+    public Vector3 Pivot; //rotate around this point in local space
+
+    public bool RotateX;
+    public bool RotateY;
+    public bool RotateZ;
+
+    public float rotationSpeed; //negative for direction change
+    public bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            transform.position += (transform.rotation * Pivot);
+
+            Vector3 rotationVector = new Vector3(RotateX ? 1f : 0f, RotateY ? 1f : 0f, RotateZ ? 1f : 0f);
+            transform.rotation *= Quaternion.AngleAxis(rotationSpeed * Time.fixedDeltaTime, rotationVector);
+
+            transform.position -= (transform.rotation * Pivot);
+
+            Vector3 rot = transform.rotation.eulerAngles;
+
+            transform.rotation = Quaternion.Euler(new Vector3(rot.x % 360f, rot.y % 360f, rot.z % 360f));
+        }
+    }
+}
