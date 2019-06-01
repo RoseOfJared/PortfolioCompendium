e113ed76c7c8d59ba85270d79356ed222903a9d7
RoseOfJared <RoseOfJared@outlook.com>
2019-06-01 13:39:20 -0400

    Tooltips Figured Out!
    -Thanks to the work of DapperDino, I have a really nice framework I can make tooltips out of, I am so happy after so goddamn long trying to find a good tutorial/starting place
    -Created basic snow shader

---
+++ Projects/CodersWorkshop/Assets/Scripts/DynamicTooltipCode/Consumable.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Text;
+
+namespace DapperDino.TooltipUI
+{
+    [CreateAssetMenu(fileName="New Consumable", menuName="Items/Consumable")]
+    public class Consumable : Item
+    {
+        [SerializeField] private Rarity rarity;
+        [SerializeField] private string useText = "Something";
+
+        public Rarity Rarity {get {return rarity;}}
+
+        public override string ColouredName
+        { 
+            get
+            {
+                string hexColour = ColorUtility.ToHtmlStringRGB(rarity.TextColour);
+                return $"<color=#{hexColour}>{Name}</color>";
+            }
+        }
+
+        public override string GetTooltipInfoText()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(Rarity.Name).AppendLine();
+            builder.Append("<color=green>Use: ").Append(useText).Append("</color>").AppendLine();
+            builder.Append("Sell Price: ").Append(SellPrice).Append(" Gold");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Projects/CodersWorkshop/Assets/Scripts/DynamicTooltipCode/Item.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+namespace DapperDino.TooltipUI
+{
+    public abstract class Item : ScriptableObject
+    {
+        [SerializeField]private new string name;
+        [SerializeField]private int sellPrice;
+
+        public string Name { get {return name;} }
+        public abstract string ColouredName{ get; }
+        public int SellPrice {get {return sellPrice;}}
+
+        public abstract string GetTooltipInfoText();
+    }
+}
